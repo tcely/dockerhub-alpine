37779dae0501f26812eaa227b0a2c95eb214bd1f
tcely <tcely@users.noreply.github.com>
2018-12-29 15:39:08 -0500

    bind: entrypoint.sh: generate rndc.key file

--- bind/entrypoint.sh
+++ bind/entrypoint.sh
@@ -8,5 +8,7 @@ chmod -c -R u+rw /var/cache/bind
 chgrp -c -R named /etc/bind
 chmod -c -R g+r /etc/bind
 
+[ -f /etc/bind/rndc.key ] || rndc-confgen -a -c /var/run/named/rndc.key -r /dev/urandom
+
 # Run in foreground and log to STDERR (console):
 exec /usr/sbin/named -c /etc/bind/named.conf -g -u named "$@"
