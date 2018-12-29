39e335b742b24deb283cfa706f3714842d99e9b3
tcely <tcely@users.noreply.github.com>
2018-12-29 15:57:04 -0500

    bind: entrypoint.sh: named needs to read rndc.key

--- bind/entrypoint.sh
+++ bind/entrypoint.sh
@@ -8,7 +8,7 @@ chmod -c -R u+rw /var/cache/bind
 chgrp -c -R named /etc/bind
 chmod -c -R g+r /etc/bind
 
-[ -f /etc/bind/rndc.key ] || rndc-confgen -a -c /var/run/named/rndc.key -r /dev/urandom
+[ -f /etc/bind/rndc.key ] || rndc-confgen -a -c /var/run/named/rndc.key -r /dev/urandom -u named
 
 # Run in foreground and log to STDERR (console):
 exec /usr/sbin/named -c /etc/bind/named.conf -g -u named "$@"
