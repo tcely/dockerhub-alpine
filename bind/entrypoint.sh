24d410ac798b20d23b253758b887ac311f554c9c
tcely <tcely@users.noreply.github.com>
2018-12-29 15:06:17 -0500

    bind: entrypoint.sh: ensure permissions

--- bind/entrypoint.sh
+++ bind/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-chown -R named /var/cache/bind
+chown -c -R root:named /var/run/named
+chmod -c ug+rwx /var/run/named
+
+chown -c -R named /var/cache/bind
+chmod -c -R u+rw /var/cache/bind
+
+chgrp -c -R named /etc/bind
+chmod -c -R g+r /etc/bind
+
 # Run in foreground and log to STDERR (console):
 exec /usr/sbin/named -c /etc/bind/named.conf -g -u named "$@"
